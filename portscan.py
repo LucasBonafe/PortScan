b45e5d4ab89ffc9e70cfd72346ef7452d9a40bf3
Lucas Bonafé <31395135+LucasBonafe@users.noreply.github.com>
2017-08-30 23:48:51 -0300

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -1,7 +1,7 @@
 import socket
 #coding=utf-8
 host = input("URL/IP: ")
-ports=[21, 22, 25, 80, 135, 443, 33006]
+ports=[21, 22, 23, 25, 53, 80, 110, 139, 443, 445, 3306, 3389, 8080]
 for port in ports:
     client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.settimeout(0.03)
@@ -10,5 +10,4 @@ for port in ports:
         print(str(port)+(" ◄ Open"))
     else:
         print(str(port)+(" • Closed"))
-
 print("Scan accomplished\n")
